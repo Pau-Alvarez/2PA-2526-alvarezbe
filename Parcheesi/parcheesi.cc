4f5bf10092e59e4fa3df0eb21ed78cac1f02d70b
alvarezbe <alvarezbe@esat-libertad.com>
2025-10-07 11:07:12 +0200

    posible final?

--- Parcheesi/parcheesi.cc
+++ Parcheesi/parcheesi.cc
@@ -52,15 +52,121 @@ void Parcheesi::PlayTurn(){
   }
 }
 
-MovementOutput Parcheesi::ApplyMovement(int piece, int mov_num){
+MovementOutput Parcheesi::ApplyMovement(int PieceIndex, int mov_num){
+ player_[current_player_].piece_[PieceIndex].position=(player_[current_player_].piece_[PieceIndex].position+mov_num)%68;
 
-  for (int i=0 ; i<mov_num ; i++){
-    if(CountPiecesOnBox(piece+i+1)==2){
-      return kMovementOutput_NoMove;
+return kMovementOutput_BasicMove;
+}
+
+//IDEAS ANTIGUAS
+
+
+/* int CheckEat(Parcheesi parchis,int dice,int active_pieces[Player::kPieceNum],int count){
+  for(int i=0 ; i<count ; i++){
+    for(int j=0 ; j<parchis.num_players-1; j++){
+      for(int k=0 ; k<Player::kPieceNum; k++){
+        if((parchis.player[parchis.currentPlayer].piece[i].pos + dice) == parchis.player[(parchis.currentPlayer+1+j)%4].piece[k].pos)
+          parchis.player[(parchis.currentPlayer+1+j)%4].piece[k].state = kHouse;
+          return i;
+      }
     }
   }
+  return NotFound;
+}
 
-  if(){
-    
+int CheckFinalLine(Player player){
+  for(int i=0 ; i<Player::kPieceNum ; i++ ){
+    if(player.piece[i].state==kFinalLine){
+      return i;
+    }
+  }
+  return NotFound;
+}
+
+int CheckBridge(Player player,int dice,int active_pieces[Player::kPieceNum],int count){
+  for(int i=0 ; i<count ; i++){
+    for(int j=0; j<count ; j++){
+      if((player.piece[i].pos + dice) == player.piece[j].pos){
+        return i;
+      }
+    } 
+  }
+  return NotFound;
+}
+
+int CheckBigmove(Player player,int active_pieces[Player::kPieceNum],int count){
+  int biggest=0;
+  int moving_piece=0;
+  for (int i=0 ; i<count ; i++){
+    if(biggest <= player.piece[i].pos){
+        biggest = player.piece[i].pos;
+        moving_piece = i;
+      }
   }
 }
+
+FinalMovement Move(Parcheesi parchis, int dice){
+  int active_pieces[Player::kPieceNum];
+  int count=0;
+  for(int i=0 ; i<Player::kPieceNum ; i++ ){
+    if(parchis.player[parchis.currentPlayer].piece[i].state==kActive){
+      active_pieces[count]=i;
+      count++;
+    }
+  }
+  int eatable=CheckEat(parchis,dice,active_pieces,count);
+  if(eatable!=NotFound){
+    FinalMovement move = {kEat , eatable};
+    return move;
+  }
+  int finish_line=CheckFinalLine(parchis.player[parchis.currentPlayer]);
+  if(finish_line!=NotFound){
+    FinalMovement move = {kFinish , finish_line};
+    return move;
+  }
+  int bridge=CheckBridge(parchis.player[parchis.currentPlayer],dice,active_pieces,count);
+  if(bridge!=NotFound){
+    FinalMovement move = {kBridge , bridge};
+    return move;
+  }
+  FinalMovement move = {kBigadvance , CheckBigmove(parchis.player[parchis.currentPlayer],active_pieces,count)};
+  return move;
+}
+
+void Turn(Parcheesi parchis){
+  const int kSpawnPlayer=5;
+  int dice_num=Dicethrow();
+  if(dice_num == kSpawnPlayer && CheckHome(parchis.player[parchis.currentPlayer]) && CheckFullStart(parchis.player[parchis.currentPlayer])){
+    int count=0;
+    while (count<= Player::kPieceNum){
+      if(parchis.player[parchis.currentPlayer].piece[count].state==kHouse){
+        parchis.player[parchis.currentPlayer].piece[count].pos=Parcheesi::startPoint[parchis.currentPlayer];
+        parchis.player[parchis.currentPlayer].piece[count].state=kActive;
+        count=Player::kPieceNum;
+      }else{
+        count++;
+      }
+    }
+  }else if(CheckAlive(parchis.player[parchis.currentPlayer])>0){
+    FinalMovement move = Move(parchis, dice_num);
+    switch (move.movement)
+    {
+    case kEat:
+      parchis.player[parchis.currentPlayer].piece[move.piece].pos+=20+dice_num;
+      if(parchis.player[parchis.currentPlayer].piece[move.piece].pos > 68){
+        parchis.player[parchis.currentPlayer].piece[move.piece].pos -=68;
+        if(parchis.player[parchis.currentPlayer].piece[move.piece].pos > Parcheesi::finishPoint[parchis.currentPlayer]){
+          int finish_pos = parchis.player[parchis.currentPlayer].piece[move.piece].pos - Parcheesi::finishPoint[parchis.currentPlayer];
+          parchis.player[parchis.currentPlayer].piece[move.piece].state = kFinalLine;
+        }
+      }
+      break;
+    
+     case kFinish:
+     break;
+
+    default:
+      break;
+    }
+  }
+}*/
